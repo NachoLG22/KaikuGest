a1605305a45a65c24c6508c8c7e3e734bd5a7e80
Nacho Lorente <ignaciolorentegarcia@gmail.com>
2023-04-17 19:59:53 +0200

    Data base config

--- api/app.js
+++ api/app.js
@@ -1,30 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const logger = require('morgan');
-const createError = require('http-errors');
+const express = require("express");
+const logger = require("morgan");
+const createError = require("http-errors");
 
+//**Load configuration*/
+require("./config/db.config");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 //** Error Handlig*/
-app.use((req, res, next) => next(createError(404,'Route not found')))
+app.use((req, res, next) => next(createError(404, "Route not found")));
 app.use((error, req, res, next) => {
   if (!error.status) {
     error = createError(500, error);
   }
-  console.error(error)
-  
+  console.error(error);
+
   const data = {
-    message: error.message
-  }
-  res.status(error.status)
-    .json(data)
-})
+    message: error.message,
+  };
+  res.status(error.status).json(data);
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.info(`Application is running at port ${port}`));
-
-
---
+++ api/config/db.config.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/kaikuGest';
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.info(`Successfully connect to the database ${MONGODB_URI}`))
+  .catch((error) => console.error('An error ocurred trying to connect to the database', error))
+
